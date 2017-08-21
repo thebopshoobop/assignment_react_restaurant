b8e47f9f739b127a2560abdfcb4bb7e35a606f07
Alex-Willenbrink <awillenbrink@gmail.com>
2017-08-21 16:24:44 -0500

    menu images and descriptions are ago

--- src/Menu.js
+++ src/Menu.js
@@ -4,17 +4,42 @@ import MenuSection from "./MenuSection";
 
 const sections = [
   {
-    name: "Wet",
+    title: "Wet",
+    textType: "info",
     products: [
-      { title: "", description: "", image: "" },
-      { title: "", description: "", image: "" }
+      {
+        title: "Chicken 'N Vegetables",
+        description: "A velvety, creamy broth with delicious real chicken and vegetables, crafted without by-products or fillers.",
+        image: "chicken-vegetables-creamy-broth.png"
+      }, {
+        title: "Tuna 'N Vegetables",
+        description: "A velvety, creamy broth with delicious real tuna and vegetables, crafted without by-products or fillers.",
+        image: "tuna-vegetables-creamy-broth.png"
+      },
+      {
+        title: "Mackerel 'N Vegetables'",
+        description: "A delicious, creamy broth with real pieces of mackerel and vegetables, crafted without by-products or fillers.",
+        image: "mackerel-vegetables-creamy-broth.png"
+      }
     ]
-  },
-  {
-    name: "Dry",
+  }, {
+    title: "Dry",
+    textType: "warning",
     products: [
-      { title: "", description: "", image: "" },
-      { title: "", description: "", image: "" }
+      {
+        title: "Seafood and Shrimp",
+        description: "Enchanting shapes with a rich, delicious melding of filet mignon and seafood flavors.",
+        image: "filet-mignon-flavor-with-real-seafood-&-shrimp.png"
+      }, {
+        title: "Fish and Salmon",
+        description: "Delicious and enchanting shapes made with real seafood.",
+        image: "ocean-fish-&-salmon-and-accents-of-garden-greens.png"
+      },
+      {
+        title: "Chicken and Turkey",
+        description: "Enchanting shapes made with the delicious taste of real chicken & turkey.",
+        image: "savory-chicken-&-turkey.png"
+      }
     ]
   }
 ];
@@ -22,9 +47,7 @@ const sections = [
 const ReservationForm = () => {
   return (
     <PageSection id="menu" type="primary" title="Our Menu">
-      {sections.map(section =>
-        <MenuSection title={section.title} products={section.products} />
-      )}
+      {sections.map(section => <MenuSection title={section.title} products={section.products} textType={section.textType}/>)}
     </PageSection>
   );
 };
--- src/MenuSection.js
+++ src/MenuSection.js
@@ -1,12 +1,13 @@
 import React from "react";
 import MenuItem from "./MenuItem";
 
-const MenuSection = ({ title, products }) => {
+const MenuSection = ({ title, products, textType }) => {
   return (
-    <section className="col-sm-4">
-      <h3>
+    <section className="col-sm-6">
+      <h3 className={`text-${textType}`}>
         {title}
       </h3>
+      <hr />
       {products.map(product =>
         <MenuItem
           title={product.title}
