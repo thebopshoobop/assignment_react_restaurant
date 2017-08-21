374ab6e79518039b83951a6f1811fa4bb2fb16ab
Alex-Willenbrink <awillenbrink@gmail.com>
2017-08-21 12:45:35 -0500

    nothing much, still trying to get navbar functioning

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,35 +2,26 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-fixed">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#menu-collapse"
-            aria-expanded="false"
-          >
-            Menu
-          </button>
-          <a className="navbar-brand" href="/">
-            Our Swanky Restaurant
-          </a>
-        </div>
+    // <nav className="navbar navbar-fixed">
+    //   <div className="container-fluid">
+    //
+    //     <div class="navbar-header">
+    //       <ul className="nav navbar-nav">
+    //         <li>
+    //           <a href="#menu">Our Menu</a>
+    //           <a href="#reservation">Make a Reservation</a>
+    //           <a href="#contact">Contact Us</a>
+    //           <a href="#map">Visit Us</a>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
+    <ul className="nav nav-tabs">
+      <li>A list item</li>
+      <li>Another list item</li>
+    </ul>
 
-        <div className="collapse navbar-collapse" id="menu-collapse">
-          <ul className="nav navbar-nav">
-            <li>
-              <a href="#menu">Our Menu</a>
-              <a href="#reservation">Make a Reservation</a>
-              <a href="#contact">Contact Us</a>
-              <a href="#map">Visit Us</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
   );
 };
 
