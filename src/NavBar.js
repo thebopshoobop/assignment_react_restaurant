304e4b100b7730af835695c7bc5336b9dc0966e8
Will Timpson <will.timpson@gmail.com>
2017-08-21 15:44:11 -0400

    iterated nav bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,27 @@
 import React from "react";
 import NavLink from "./NavLink";
+
+const links = [
+  { target: "about", text: "Our Restaurant" },
+  { target: "menu", text: "Our Menu" },
+  { target: "reservation", text: "Make a Reservation" },
+  { target: "map", text: "Visit Us" },
+  { target: "contact", text: "Contact Us" }
+];
+
 const NavBar = () => {
   return (
     <nav className="navbar navbar-default">
       <div className="container-fluid">
         <div className="navbar-header">
           <ul className="nav navbar-nav">
-            <NavLink target="about" text="Our Restaurant" />
-            <NavLink target="menu" text="Our Menu" />
-            <NavLink target="reservation" text="Make a Reservation" />
-            <NavLink target="map" text="Visit Us" />
-            <NavLink target="contact" text="Contact Us" />
+            {links.map(link =>
+              <NavLink
+                target={link.target}
+                text={link.text}
+                key={link.target}
+              />
+            )}
           </ul>
         </div>
       </div>
